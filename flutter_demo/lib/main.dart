aa69af9be78788a476e4c65ccead3e4c69518e2f
crq <2631315464@qq.com>
2022-09-21 07:36:08 +0800

    day07打卡

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/class3/route/page_one.dart';
+import 'package:flutter_demo/class3/route/page_three.dart';
+import 'package:flutter_demo/class3/route/unknown_page.dart';
 // import 'package:flutter_demo/feign/screens/main_screen.dart';
 // import 'package:flutter_demo/basic/text_widget.dart';
 // import 'package:flutter_demo/basic/image_widget.dart';
@@ -31,10 +34,10 @@ import 'package:flutter/material.dart';
 // import 'package:flutter_demo/basic/form_widget.dart';
 // import 'package:flutter_demo/basic/date_demo.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:scoped_model/scoped_model.dart';
 
-import 'class2/theme_state_model.dart';
-import 'day21/day06.dart';
+import 'class3/count/count_page.dart';
+import 'class3/route/page_two.dart';
+import 'day21/day07.dart';
 
 void main() {
   runApp(const MyApp());
@@ -46,96 +49,104 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    List themeList = [
-      ThemeData(
-        brightness: Brightness.light,
-        // primarySwatch: Colors.red,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.cyan,
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.red, //设置icon主题色为黄色
-          size: 32, // 设置icon大小
-        ),
-        textTheme: TextTheme(
-          headline4: TextStyle(color: Colors.cyan.shade900), // 设置标题4号文本颜色为红色
-        ),
-        //分割线颜色
-        dividerColor: Colors.cyan.shade600,
-        scaffoldBackgroundColor: Colors.cyan,
-        textButtonTheme: TextButtonThemeData(
-          // 按钮主题
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.blue,
-            side: const BorderSide(
-              color: Colors.blue,
-            ),
-          ),
-          // ButtonStyle(
-          // backgroundColor: MaterialStatePropertyAll(Colors.cyan.shade800),
-          // foregroundColor: const MaterialStatePropertyAll(Colors.white),
-          // ),
-        ),
-      ),
-      ThemeData(
-        brightness: Brightness.light,
-        // primarySwatch: Colors.red,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.greenAccent,
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.red, //设置icon主题色为黄色
-          size: 32, // 设置icon大小
-        ),
-        textTheme: TextTheme(
-          headline4: TextStyle(color: Colors.green.shade900), // 设置标题4号文本颜色为红色
-        ),
-        //分割线颜色
-        dividerColor: Colors.green.shade600,
-        scaffoldBackgroundColor: Colors.greenAccent,
-        textButtonTheme: TextButtonThemeData(
-          // 按钮主题
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.greenAccent,
-            side: const BorderSide(
-              color: Colors.greenAccent,
-            ),
-          ),
-          // ButtonStyle(
-          // backgroundColor: MaterialStatePropertyAll(Colors.cyan.shade800),
-          // foregroundColor: const MaterialStatePropertyAll(Colors.white),
-          // ),
-        ),
-      ),
-    ];
+    // List themeList = [
+    //   ThemeData(
+    //     brightness: Brightness.light,
+    //     // primarySwatch: Colors.red,
+    //     appBarTheme: const AppBarTheme(
+    //       backgroundColor: Colors.cyan,
+    //     ),
+    //     iconTheme: const IconThemeData(
+    //       color: Colors.red, //设置icon主题色为黄色
+    //       size: 32, // 设置icon大小
+    //     ),
+    //     textTheme: TextTheme(
+    //       headline4: TextStyle(color: Colors.cyan.shade900), // 设置标题4号文本颜色为红色
+    //     ),
+    //     //分割线颜色
+    //     dividerColor: Colors.cyan.shade600,
+    //     scaffoldBackgroundColor: Colors.cyan,
+    //     textButtonTheme: TextButtonThemeData(
+    //       // 按钮主题
+    //       style: TextButton.styleFrom(
+    //         foregroundColor: Colors.blue,
+    //         side: const BorderSide(
+    //           color: Colors.blue,
+    //         ),
+    //       ),
+    //       // ButtonStyle(
+    //       // backgroundColor: MaterialStatePropertyAll(Colors.cyan.shade800),
+    //       // foregroundColor: const MaterialStatePropertyAll(Colors.white),
+    //       // ),
+    //     ),
+    //   ),
+    //   ThemeData(
+    //     brightness: Brightness.light,
+    //     // primarySwatch: Colors.red,
+    //     appBarTheme: const AppBarTheme(
+    //       backgroundColor: Colors.greenAccent,
+    //     ),
+    //     iconTheme: const IconThemeData(
+    //       color: Colors.red, //设置icon主题色为黄色
+    //       size: 32, // 设置icon大小
+    //     ),
+    //     textTheme: TextTheme(
+    //       headline4: TextStyle(color: Colors.green.shade900), // 设置标题4号文本颜色为红色
+    //     ),
+    //     //分割线颜色
+    //     dividerColor: Colors.green.shade600,
+    //     scaffoldBackgroundColor: Colors.greenAccent,
+    //     textButtonTheme: TextButtonThemeData(
+    //       // 按钮主题
+    //       style: TextButton.styleFrom(
+    //         foregroundColor: Colors.greenAccent,
+    //         side: const BorderSide(
+    //           color: Colors.greenAccent,
+    //         ),
+    //       ),
+    //       // ButtonStyle(
+    //       // backgroundColor: MaterialStatePropertyAll(Colors.cyan.shade800),
+    //       // foregroundColor: const MaterialStatePropertyAll(Colors.white),
+    //       // ),
+    //     ),
+    //   ),
+    // ];
     final _navKey = GlobalKey<NavigatorState>();
 
-    final List<double> doubleList = [2.0, 5.0, 8.0, 9.0, 2.0, 4.0];
+    // final List<double> doubleList = [2.0, 5.0, 8.0, 9.0, 2.0, 4.0];
 
-    return ScopedModel<ThemeStateModel>(
-      model: ThemeStateModel(),
-      child: ScopedModelDescendant<ThemeStateModel>(
-        builder: (context, child, model) {
-          return MaterialApp(
-            navigatorKey: _navKey,
-            debugShowCheckedModeBanner: false,
-            title: 'Flutter UIs',
-            // theme: themeList[model.themeIndex],
-            localizationsDelegates: const [
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate
-            ],
-            supportedLocales: const [Locale('zh', 'CH'), Locale('en', 'US')],
-            // home: PaintPage(doubleList: doubleList),
-            home: const TodoHomeTwoPage(),
-            // onGenerateRoute: onGenerateRoute,
-            // routes: {
-            //   '/': (context) => const NewsPage(),
-            //   '/news-content': (context, {arguments}) => NewsContent(arguments: arguments),
-            // },
-          );
-        },
-      ),
+    // return ScopedModel<ThemeStateModel>(
+    //   model: ThemeStateModel(),
+    //   child: ScopedModelDescendant<ThemeStateModel>(
+    //     builder: (context, child, model) {
+    return MaterialApp(
+      routes: {
+        'page_one': (context) => const PageOne(),
+        'page_two': (context) => const PageTwo(),
+        'page_three': (context) => const PageThree(),
+        'page_count': (context) => const CountAddPage(),
+      },
+      onUnknownRoute: (RouteSettings setting) =>
+          MaterialPageRoute(builder: (context) => const UnknownPage()),
+      navigatorKey: _navKey,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter UIs',
+      // theme: themeList[model.themeIndex],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: const [Locale('zh', 'CH'), Locale('en', 'US')],
+      // home: PaintPage(doubleList: doubleList),
+      home: const LoginSevenPage(),
+      // onGenerateRoute: onGenerateRoute,
+      // routes: {
+      //   '/': (context) => const NewsPage(),
+      //   '/news-content': (context, {arguments}) => NewsContent(arguments: arguments),
+      // },
     );
+    //     },
+    //   ),
+    // );
   }
 }
